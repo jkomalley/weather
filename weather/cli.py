f42ee5f4aac790997eb8e53539b494926575e6d3
Kyle O'Malley <j.kyle.omalley@gmail.com>
2024-02-16 21:32:10 -0500

    built weather alert requests

--- weather/cli.py
+++ weather/cli.py
@@ -1,5 +1,6 @@
 import click
 from weather import weather
+from rich import print
 
 DEGREE_SIGN = chr(176)
 
@@ -27,7 +28,9 @@ DEGREE_SIGN = chr(176)
     help="Display the upcoming forecast.",
 )
 def run(display):
-    w = weather.Weather(39.215833, -76.709167)
+    lat = 39.215833
+    lon = -76.709167
+    w = weather.Weather(lat, lon)
 
     if display == "tempurature":
         t = w.getCurrentTemperature()
@@ -42,10 +45,25 @@ def run(display):
         print(f"{f}")
 
     else:
+        alerts = w.getAlerts()
         t = w.getCurrentTemperature()
         c = w.getConditions()
+        wc = 43.0 # w.getWindChill()
+        h = 0.29 # w.getHumidity()
+        d = 16.0 # w.getDewPoint()
+        ws = "W 9 G 20 mph" # w.getWindSpeed()
+        lu = "16 February 2:54 pm EST" # w.getLastUpdateTime()
+
+        print(f"[green]Weather near {lat},{lon}[/green]")
+        for alert in alerts:
+            print(f"[bold red]Alert: {alert}[/bold red]")
+        print(f" Conditions: {c.capitalize()}")
         print(f"Tempurature: {t:.2f}{DEGREE_SIGN}F")
-        print(f"Weather: {c}")
+        print(f" Wind Chill: {wc:.2f}{DEGREE_SIGN}F")
+        print(f"   Humidity: {h * 100:.2f}%")
+        print(f"  Dew Point: {d:.2f}{DEGREE_SIGN}F")
+        print(f" Wind Speed: {ws}")
+        print(f"Last Update: {lu}")
 
 
 if __name__ == "__main__":
--- weather/weather.py
+++ weather/weather.py
@@ -1,6 +1,6 @@
 import requests
-import json
-import datetime
+# import json
+# import datetime
 
 
 class Weather:
@@ -16,6 +16,7 @@ class Weather:
     GET_HOURLY_FORECAST_DATA = "/gridpoints/{wfo}/{x},{y}/forecast/hourly"
     GET_STATIONS = "/gridpoints/{wfo}/{x},{y}/stations"
     GET_LATEST_STATION_OBSERVATION = "/stations/{stationId}/observations/latest"
+    GET_ACTIVE_ALERTS = "/alerts/active/zone/{zoneId}"
 
     def __init__(self, latitude, longitude):
         self.properties = self._getLocationMetadata(latitude, longitude)
@@ -23,6 +24,42 @@ class Weather:
         self.gridID = self.properties["gridId"]
         self.gridX = self.properties["gridX"]
         self.gridY = self.properties["gridY"]
+    
+    def getAlerts(self):
+        ret = []
+
+        # parse zone id from location properties
+        zoneId = self.properties["forecastZone"].split("/")[-1]
+
+        print(f"{zoneId = }")
+
+        url = self.NWS_API_ADDRESS + self.GET_ACTIVE_ALERTS.format(
+            zoneId=zoneId
+        )
+
+        # print(f"{url = }")
+
+        headers = {"user_agent": self.USER_AGENT, "accept": "application/ld+json"}
+
+        response = requests.get(url, headers=headers)
+
+        try:
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            print(f"Error: {str(e)}")
+            exit()
+
+        # print(json.dumps(response.json(), indent=4))
+        
+        rawAlerts = response.json()["@graph"]
+        
+        for rawAlert in rawAlerts:
+            print(rawAlert)
+            print(type(rawAlert))
+            alert = rawAlert["parameters"]["NWSheadline"][0]
+            ret.append(alert)
+
+        return ret
 
     def getCurrentTemperature(self):
         # get station id
@@ -54,9 +91,6 @@ class Weather:
     def getForecast(self):
         return "Idk maybe some rain"
 
-    def getAlerts(self):
-        return "AHHHHHHH WE'RE ALL GONNA DIE"
-
     def _getHourlyData(self):
         url = self.NWS_API_ADDRESS + self.GET_HOURLY_FORECAST_DATA.format(
             wfo=self.gridID, x=self.gridX, y=self.gridY
@@ -79,6 +113,9 @@ class Weather:
         return data
 
     def _getLatestObservation(self, stationId):
+        if hasattr(self, "_latestObservation"):
+            return self._latestObservation
+        
         url = self.NWS_API_ADDRESS + self.GET_LATEST_STATION_OBSERVATION.format(
             stationId=stationId
         )
@@ -94,10 +131,15 @@ class Weather:
             exit()
 
         # print(json.dumps(response.json(), indent=4))
+        
+        self._latestObservation = response.json()
 
-        return response.json()
+        return self._latestObservation
 
     def _getStationId(self):
+        if hasattr(self, "_stationId"):
+            return self._stationId
+
         url = self.NWS_API_ADDRESS + self.GET_STATIONS.format(
             wfo=self.gridID, x=self.gridX, y=self.gridY
         )
@@ -120,7 +162,9 @@ class Weather:
 
         stationId = response.json()["@graph"][0]["stationIdentifier"]
 
-        return stationId
+        self._stationId = stationId
+
+        return self._stationId
 
     def _getLocationMetadata(self, latitude, longitude):
         url = self.NWS_API_ADDRESS + self.GET_POINT_METADATA.format(
